2c7bbbd194edd375a1b9df0a4f22a3da5526c9dd
belarbic <}belarbic@users.noreply.github.com>
2024-09-16 14:49:18 -0400

    modified  src/MultiSet.java

--- src/MultiSet.java
+++ src/MultiSet.java
@@ -10,5 +10,8 @@ public abstract class MultiSet {
      */
     abstract void add(Integer item);
 
+    abstract void remove(Integer item);
+
+    abstract void contains(Integer item);
     // TODO finish adding abstract methods to fully describe what it means to be a MultiSet.
 }
