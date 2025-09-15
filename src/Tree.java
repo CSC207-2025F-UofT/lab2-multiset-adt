6463c6282c796f7cb436f4da487ce8bd23e2dddc
blobcode <lhelme123@gmail.com>
2025-09-15 17:00:02 -0400

    Create Base Class for Tree

--- src/Tree.java
+++ src/Tree.java
@@ -1,6 +1,19 @@
+import java.util.ArrayList;
+
 public class Tree {
     // We recommend attempting this class last, as it hasn't been scaffolded for your team.
     // Even if your team doesn't have time to implement this class, it is a useful exercise
     // to think about how you might split up the work to get the Tree and TreeMultiSet
     // implemented.
+    private int root;
+    private ArrayList<Tree> subtrees;
+
+    void Tree(int root, ArrayList<Tree> subtrees) {
+        this.root = root;
+        if (subtrees == null) {
+            this.subtrees = new ArrayList<>();
+        } else {
+            this.subtrees = subtrees;
+        }
+    }
 }
