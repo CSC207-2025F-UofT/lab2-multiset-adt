b8e9e30dd3b3864e3c364d3f27b1c0038f49d11c
tiffany_lin <tiffanyg.lin@mail.utoronto.ca>
2025-09-16 01:41:53 -0400

    LinkedListMultiSet finished

--- src/LinkedListMultiSet.java
+++ src/LinkedListMultiSet.java
@@ -6,6 +6,7 @@ class Node {
     Node next;
     Node(int item) {
         this.item = item;
+        this.next = null;
     }
 }
 
@@ -25,23 +26,51 @@ public class LinkedListMultiSet extends MultiSet {
     }
 
     public void remove(int item) {
-
+        Node cur = front;
+        Node prev = null;
+        while (cur != null) {
+            if (cur.item == item) {
+                size--;
+                if (prev != null) {
+                    prev.next = cur.next;
+                }
+                else {
+                    front = cur.next;
+                }
+            }
+            prev = cur;
+            cur = cur.next;
+        }
     }
 
     public boolean contains(int item) {
+        Node cur = front;
+        while (cur != null) {
+            if (cur.item == item){
+                return true;
+            }
+            cur = cur.next;
+        }
         return false;
     }
 
     public boolean isEmpty() {
-        return false;
+        return (front == null);
     }
 
-
     public int count(int item) {
-        return -1;
+        int count = 0;
+        Node cur = front;
+        while (cur != null) {
+            if (cur.item == item) {
+                count++;
+            }
+            cur = cur.next;
+        }
+        return count;
     }
 
     public int size() {
-        return -1;
+        return size;
     }
 }
